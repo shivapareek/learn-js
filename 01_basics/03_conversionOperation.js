1d0bcb7b36a361debf9a723c7e23b4fbc2384e6c
shivapareek <pareekshiva2004@gmail.com>
2025-06-09 22:29:58 +0530

    more about operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,4 +32,45 @@ let BooleanIsLoggedInNumber = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber); // "33"
+// console.log(stringNumber); // "33"
+
+
+
+// **************************** Operations ******************************
+
+let value = 3
+let negValue = -value; // -3
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2; // "hello world"
+// console.log(str3); // "hello world"
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + 2 + "2");  // "32"
+// console.log("1" + 2 + "2"); // "122"
+
+console.log(true); //true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 10;
+
+
+let gameCounter = 100;
+++gameCounter; // 101
+// console.log(gameCounter); // 101
+
+//link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
