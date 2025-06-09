fe04532bc261e3935f0af58c6793143350d6f192
shivapareek <pareekshiva2004@gmail.com>
2025-06-09 18:43:14 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "shiva";
+
+// console.log(typeof score); 
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log( valueInNumber); 
+
+// 33 => 33
+// "33abc" => NaN
+// "33" => 33   
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// "3.14" => 3.14
+// "3.14abc" => NaN
+
+
+let isLoggedIn = "shiva";
+let BooleanIsLoggedInNumber = Boolean(isLoggedIn);
+//console.log(BooleanIsLoggedInNumber); //1
+
+// 1 => true
+// 0 => false   
+// "" => false
+// "shiva" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "33"
