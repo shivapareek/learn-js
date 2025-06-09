e822c93a72f48b156ea4691db295208df0496b5e
shivapareek <pareekshiva2004@gmail.com>
2025-06-09 18:19:15 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict" //treat all JS code as newer version
+
+
+//alert (3+3); //we are using node.js, not browser, so no alert
+
+console.log(3
+    +3); // code readibility is important, so we can break lines
+
+    console.log("shiva"); 
+
+
+let name = "shiva"; //string
+let age = 25; //number
+let isStudent = true; //boolean
+let state = null;
+
+
+
+
+//number = 3.14, 123, -200, 0, Infinity, NaN
+//bigint = 1234567890123456789012345678901234567890n
+//string = "shiva"
+//boolean = true/false  
+//null = no value
+//undefined = variable declared but not assigned a value    
+//symbol = unique
+
+
+
+//object
+
+
+console.log(typeof null); 
+console.log(typeof undefined); //undefined
+console.log(typeof 3.14); //number
