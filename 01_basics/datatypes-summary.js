763b1ebd2c447862a902d5166e2b688775be8cfa
shivapareek <pareekshiva2004@gmail.com>
2025-06-10 15:39:19 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+// 7 types - Number, String, Boolean, Undefined, Null, Symbol, BigInt       
+
+const score = 100; // Number
+const name = "John"; // String
+const isActive = true; // Boolean
+const age = undefined; // Undefined
+const address = null; // Null
+const uniqueId = Symbol('id'); // Symbol
+
+
+// Refrence (Non Primitive)
+
+// Array,Object, Function
+
+const heros = ["Ironman", "Spiderman", "Hulk"]; // Array
+const person = { name: "John", age: 30 }; // Object
+
+const myVar = function greet() { // Function
+    console.log("Hello, World!");
+}
