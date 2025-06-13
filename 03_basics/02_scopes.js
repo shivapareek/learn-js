4179f8d6f6aaa53d7c5a38629da47ec460b67e68
shivapareek <pareekshiva2004@gmail.com>
2025-06-13 22:05:08 +0530

    more about scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,5 @@
+
+
 let a =400;
 const b = 500;
 var c = 600;
@@ -7,9 +9,46 @@ if (true) {
     let a = 100; // block scoped
     const b = 200; // block scoped
     var c = 300; // function scoped
-    console.log(a); // 100
+    // console.log(a); // 100
 }
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = "shiva"
+
+    function two(){
+        const website = "shiva.com";
+    console.log(username); // shiva
+}
+// console.log(website); // shiva.com
+two()
+}
+// one()
+
+
+if (true) {
+   const username = "shiva";
+   if (username === "shiva") {
+       const website = "shiva.com";
+    //    console.log(username + website); // shiva.com
+   }
+//    console.log(website); 
+}
+// console.log(username); // ReferenceError: username is not defined
+
+// +++++++++++++++++++++++ interseting +++++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num+1;
+}
+
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(5)
