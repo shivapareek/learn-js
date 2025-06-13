a4adb0c6a9b81b6c29f7d99c0d9254a817b8251a
shivapareek <pareekshiva2004@gmail.com>
2025-06-14 00:13:04 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,31 @@
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby'
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut is for ${myObj[key]}`);
+    
+}
+
+
+const programming =  ["js" , "rb" , "py" , "java" , "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA' , "America")  
+// map.set('FR' , "France")
+// map.set('FR' , "France")
+
+// for (const key in map) {
+// console.log(map); // not iterable
+
+// }
+
+
