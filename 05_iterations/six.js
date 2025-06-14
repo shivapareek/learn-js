b196811fb13038af4426c720fd49b4ff246cc3a5
shivapareek <pareekshiva2004@gmail.com>
2025-06-14 13:27:01 +0530

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+// const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+// const values = coding.forEach((item) => {
+//     //console.log(item);
+//     return item;
+// })
+
+// console.log(values)
+
+const myNums = [1,2,3,4,5];
+
+// const newNums = myNums.filter((num) => {
+//     return  num >4
+    
+// })
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num>4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums)
+
