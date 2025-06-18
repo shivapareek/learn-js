0b7b9fe79f8e070a620e8a6e33a1f65e93b90fc2
shivapareek <pareekshiva2004@gmail.com>
2025-06-18 23:14:40 +0530

    oop

--- 09_classes_and_oop/oop.js
+++ 09_classes_and_oop/oop.js
@@ -4,10 +4,33 @@ const user = {
     signedIn: true,
 
     getUserDetails: function(){
-        console.log("Got user details from database");
+        // console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this)
         
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+function User(username , loginCount , isloggedin){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isloggedin = isloggedin;
+
+    this.greeting = function (){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    // return this
+}
+
+const UserOne = new User("shiva" , 12 , true)
+const UserTwo = new User("pareek" , 12 , true)
+console.log(UserOne.constructor);
+// console.log(UserTwo);
+
+
